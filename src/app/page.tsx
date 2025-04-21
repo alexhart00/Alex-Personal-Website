bd018add1b5f1714caf789be52bbebed89cbe3b2
Alexander D Hart <alexhart3000@gmail.com>
2025-04-20 21:46:42 -0600

    Add profile Page
    making a profile page, with a profile image. Have to remember all of the stuff required to make react work, but this is something, right?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <Image src="/ProfilePic.jpg" alt="Profile Picture" width={400} height={200}/>
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -24,7 +25,7 @@ export default function Home() {
             Save and see your changes instantly.
           </li>
         </ol>
-        <div>How are you doing my kind sir</div>
+        <div><a href="profile.tsx">Alex's Profile Link</a></div>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
