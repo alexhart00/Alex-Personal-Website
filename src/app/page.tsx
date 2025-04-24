659d44f03cb3aa4c8be355822bc5d16ddd24794f
Alexander D Hart <alexhart3000@gmail.com>
2025-04-23 21:57:36 -0600

    About page initialization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -25,7 +26,7 @@ export default function Home() {
             Save and see your changes instantly.
           </li>
         </ol>
-        <div><a href="profile.tsx">Alex's Profile Link</a></div>
+        <div><Link href="about">Alex's Profile Link</Link></div>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
