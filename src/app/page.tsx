e32649258bd3053eccbc58d92835774c14ad2d08
Alexander D Hart <alexhart3000@gmail.com>
2025-04-25 13:26:59 -0600

    profile pressing and cycling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,12 @@ import { useState } from 'react'
 
 export default function Home() {
 
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const maxImages = 6;
+
+  const handleClick = () => {
+    setCount((prevCount) => (prevCount + 1) % maxImages);
+  };
   
 
   const handleClickScroll = (id: string) => {
@@ -17,40 +22,38 @@ export default function Home() {
   };
 
   return (
-    
     <div>
-    <nav>
-      <ul className="flex-wrap">
-        <li onClick={() => handleClickScroll("Profile")}>Profile</li>
-        <li onClick={() => handleClickScroll("WorkExperience")}>Work Experience</li>
-        <li onClick={() => handleClickScroll("Projects")}>Projects</li>
-        <li onClick={() => handleClickScroll("Social")}>Social</li>
-      </ul>
-    </nav>
-    <main className="flex justify-start gap-[24px]">
-    <div id="Profile">
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}><Image src ="/PlaceHolder1.png" width={200} height={300} alt="Profile Picture"></Image></button>
+      <nav>
+        <ul className="flex-wrap">
+          <li onClick={() => handleClickScroll("Profile")}>Profile</li>
+          <li onClick={() => handleClickScroll("WorkExperience")}>Work Experience</li>
+          <li onClick={() => handleClickScroll("Projects")}>Projects</li>
+          <li onClick={() => handleClickScroll("Social")}>Social</li>
+        </ul>
+      </nav>
+      <main className="flex justify-start gap-[24px]">
+      <div id="Profile">
+        <p>You clicked {count} times</p>
+        <button onClick={handleClick}><Image src={`/PlaceHolder${count}.png`} width={400} height={600} alt="Profile Picture" /></button>
 
-    </div>
-    <div id="WorkExperience">
-
-    </div>
-    <div id="Projects">
-
-    </div>
-    </main>
-    <footer className="flex gap-[24px]">
-      <div id="Social">
-        <Link href="https://www.linkedin.com/in/josh-o-scott/" prefetch={false}>
-          <Image src = "https://www.linkedin.com/favicon.ico" width={60} height={60} alt="linkedin Logo"/>
-        </Link>
-        <Link href="https://www.facebook.com/profile.php?id=100009625337073" prefetch={false}>
-          <Image src = "https://www.facebook.com/favicon.ico" width={60} height={60} alt="Facebook Logo"/>
-        </Link>
       </div>
-    </footer>
-        
+      <div id="WorkExperience">
+
+      </div>
+      <div id="Projects">
+
+      </div>
+      </main>
+      <footer className="flex gap-[24px]">
+        <div id="Social">
+          <Link href="https://www.linkedin.com/in/josh-o-scott/" prefetch={false}>
+            <Image src = "https://www.linkedin.com/favicon.ico" width={60} height={60} alt="linkedin Logo"/>
+          </Link>
+          <Link href="https://www.facebook.com/profile.php?id=100009625337073" prefetch={false}>
+            <Image src = "https://www.facebook.com/favicon.ico" width={60} height={60} alt="Facebook Logo"/>
+          </Link>
+        </div>
+      </footer>
     </div>
   );
 }
