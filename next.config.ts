132cd3d633108b4c2616f13782199fb435f1e33f
Alexander D Hart <alexhart3000@gmail.com>
2025-04-25 13:04:03 -0600

    PlaceHolder Pictures

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,11 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+  images: {
+    remotePatterns: [new URL('https://www.facebook.com/favicon.ico'), new URL('https://www.linkedin.com/favicon.ico')],
+    
+  },
+}
+
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,57 @@
+'use client'
+
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from 'react'
 
 export default function Home() {
+
+  const [count, setCount] = useState(0)
+  
+
+  const handleClickScroll = (id: string) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-      </footer>
+    
+    <div>
+    <nav>
+      <ul className="flex-wrap">
+        <li onClick={() => handleClickScroll("Profile")}>Profile</li>
+        <li onClick={() => handleClickScroll("WorkExperience")}>Work Experience</li>
+        <li onClick={() => handleClickScroll("Projects")}>Projects</li>
+        <li onClick={() => handleClickScroll("Social")}>Social</li>
+      </ul>
+    </nav>
+    <main className="flex justify-start gap-[24px]">
+    <div id="Profile">
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}><Image src ="/PlaceHolder1.png" width={200} height={300} alt="Profile Picture"></Image></button>
+
+    </div>
+    <div id="WorkExperience">
+
+    </div>
+    <div id="Projects">
+
+    </div>
+    </main>
+    <footer className="flex gap-[24px]">
+      <div id="Social">
+        <Link href="https://www.linkedin.com/in/josh-o-scott/" prefetch={false}>
+          <Image src = "https://www.linkedin.com/favicon.ico" width={60} height={60} alt="linkedin Logo"/>
+        </Link>
+        <Link href="https://www.facebook.com/profile.php?id=100009625337073" prefetch={false}>
+          <Image src = "https://www.facebook.com/favicon.ico" width={60} height={60} alt="Facebook Logo"/>
+        </Link>
+      </div>
+    </footer>
+        
     </div>
   );
 }
+
