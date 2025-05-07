ca55da9336c121c59fb2eac7ca50994fc9f89d9d
Alexander D Hart <alexhart3000@gmail.com>
2025-05-07 13:52:03 -0600

    remove speedInsight

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 'use client'
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 import Image from "next/image";
