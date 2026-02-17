75d24cc1fbd4eab1f8429b7fdf693f286c39ae80
Alexander D Hart <alexhart3000@gmail.com>
2026-02-17 11:46:12 -0700

    chore: update dependencies and improve TypeScript configuration
    - Updated Next.js to version 16.1.6 and React to version 19.2.4 in package.json
    - Removed unused profile pictures from public directory
    - Adjusted Profile component to limit profile images to 3
    - Enhanced tsconfig.json for better JSX support and included additional type definitions

--- src/app/components/Profile.tsx
+++ src/app/components/Profile.tsx
@@ -1,17 +1,17 @@
 import { useState } from "react";
 import Image from "next/image";
 
-export function ProfileComponent(){
-  const maxImages = 5;
+export function ProfileComponent() {
+  const maxImages = 3;
   const [profilePhotoIndex, setProfilePhotoIndex] = useState(0);
   const handleClick = () => setProfilePhotoIndex((prevIndex) => (prevIndex + 1) % maxImages);
 
   return (
     <header className="flex flex-col  bg-zinc-900 p-12 outline-6 outline-yellow-600 drop-shadow-md  bg-opacity-10 bg-cover bg-center bg-[url(/BackgroundTemp.jpg)] shadow-yellow-600 shadow-xl relative z-10">
-        <div className="flex flex-col items-center">
-          <h1 className="text-5xl md:text-5xl font-bold p-4">Alexander Hart</h1>
-          <button onClick={handleClick} className ="outline-6  outline-yellow-600 shadow-yellow-600 shadow-xl"><div className="w-full max-w-[400px] h-auto sm:h-[500px]"><Image className="object-fill w-full min-w-[375px]  max-w-[375px] h-auto sm:h-[500px]" src={`/profilePicture(${profilePhotoIndex}).png`} width={400} height={600} alt="Profile Picture" /></div></button>
-        </div>
+      <div className="flex flex-col items-center">
+        <h1 className="text-5xl md:text-5xl font-bold p-4">Alexander Hart</h1>
+        <button onClick={handleClick} className="outline-6  outline-yellow-600 shadow-yellow-600 shadow-xl"><div className="w-full max-w-[400px] h-auto sm:h-[500px]"><Image className="object-fill w-full min-w-[375px]  max-w-[375px] h-auto sm:h-[500px]" src={`/profilePicture(${profilePhotoIndex}).png`} width={400} height={600} alt="Profile Picture" /></div></button>
+      </div>
     </header>
   );
 }
